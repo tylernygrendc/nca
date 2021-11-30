997056ef71a161c3b3e416346941345090e9b654
Tyler Nygren <tylernygren@gmail.com>
2021-11-30 16:58:01 -0500

    restructure

--- public/js/layout.js
+++ public/js/layout.js
@@ -1,7 +1,5 @@
-window.addEventListener('load', () => {
-    if(window.innerWidth < 600){
-        document.querySelector('#site-title').innerText = "NCA";
-    } else {
-        document.querySelector('#site-title').innerText = "Nygren Chiropractic and Acupuncture";
-    } 
-});
+if(window.innerWidth < 600){
+    document.querySelector('#site-title').innerText = "NCA";
+} else {
+    document.querySelector('#site-title').innerText = "Nygren Chiropractic and Acupuncture";
+}
